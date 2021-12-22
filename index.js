a74998362bdaa1b810a5577df0c1a6044973a424
ilyosbek <ilyosbek.inomjonov.1996@gmail.com>
2021-12-22 19:51:07 +0500

    done

---
+++ index.js
@@ -0,0 +1,7 @@
+function app(param) {
+    if (param === 'Uzbek') console.log('Assalomu aleykum');
+    else if (param === 'Rus') console.log('Привет');
+    else if (param === 'Eng') console.log('Hello');
+    else console.log('Bizda bunaqangi til mavjud emas');
+}
+app('Turk')
